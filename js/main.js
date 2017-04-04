daf8f9f4b2200fbff6078461e639fe1eac8f26a7
Scott Travers <Scott.M.Travers@gmail.com>
2017-04-04 12:35:16 -0700

    Add video carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Bootstrap Smooth Scroll
-$("a").on('click', function(event) {
+$("#fixed-navbar a").on('click', function(event) {
   // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
     // Prevent default anchor click behavior
